0abae358bd9ba76c690b7719299011bb036fc734
Matteo Joliveau <m.joliveau@koor.it>
2017-06-01 17:19:38 +0200

    First commit

---
+++ appsdk/src/main/java/org/plugface/demo/appsdk/Strings.java
@@ -0,0 +1,7 @@
+package org.plugface.demo.appsdk;
+
+public interface Strings {
+    Integer execute(String string);
+    String invert(String string);
+    String[] split(String string, String regex);
+}
